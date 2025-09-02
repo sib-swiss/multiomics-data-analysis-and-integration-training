f7fc82d520b9c5cda5249a3898aa6a879d02b007
Florence Mehl <florence.mehl@isb-sib.ch>
2025-09-02 15:41:13 +0200

    logistics update

--- packages_installation.R
+++ packages_installation.R
@@ -1,3 +1,7 @@
+# R version 4.5.1 (2025-06-13)
+
+# Packages to install
+
 install.packages("MBAnalysis")
 install.packages("CCA")
 install.packages("ggplot2")
@@ -9,3 +13,4 @@ install.packages("factoextra")
 install.packages("ConsensusOPLS")
 BiocManager::install("mixOmics")
 BiocManager::install("ropls")
+BiocManager::install("BiocParallel")
